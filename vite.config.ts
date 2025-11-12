e9d57eee6aeff81eb0fee799061131686a0beae6
LiliaBekrar <Liliarossi2504@gmail.com>
2025-11-12 10:50:38 +0100

    correction github action

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/wishlists/', // ⬅️ remplace par le nom de ton repo
-    plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  plugins: [react()],
 })
